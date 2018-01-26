ca93635791c137db47292970f1730a08b1e16331
Anton Gulenko <anton.gulenko@googlemail.com>
2018-01-26 17:07:37 +0100

    Pulled parts of the Clearwater heat scripts into a Common folder for reuse

---
+++ Common/heat-create.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Work in the current directory
+home="."
+
+HEAT_TEMPLATE="$1"
+DEFAULT_NAME="$2"
+shift 2
+
+case $# in
+    0) HEAT_CMD="create"; NAME="$DEFAULT_NAME";;
+    1) HEAT_CMD="create"; NAME="$1";;
+    2) HEAT_CMD="$1"; NAME="$2";;
+    *) echo "Parameters: [command] [stack-name]"; exit 1;;
+esac
+
+PARM="$home/parameters.txt"
+test -e "$PARM" || { echo "The required file '$PARM' does not exist. Create it based on 'parameters.txt.template'"; exit 1; }
+
+HEAT_CMD="stack $HEAT_CMD"
+echo "openstack $HEAT_CMD with name $NAME"
+
+# Create sub-dir for keys
+KEYS="$home/keys"
+mkdir -p "$KEYS"
+# Create key pair that will be used by the VMs to access each other
+echo -e 'y\n' | ssh-keygen -q -t rsa -b 4096 -f "$KEYS/id_rsa" -N '' 1>/dev/null
+
+openstack $HEAT_CMD "$NAME" -t "$HEAT_TEMPLATE" $(cat "$PARM")
