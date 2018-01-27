5caf4e6f58742959d42b342ce73b1d7de95813d1
Anton Gulenko <anton.gulenko@googlemail.com>
2018-01-27 23:01:17 +0100

    Small cleanups and refactorings in the swarm initialization scripts. Moved 1 script to the Common folder. Hope not broken.

---
+++ Common/docker-swarm-initialize-remote.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Expected environment variables in this script:
+# LC_STACK_NAME: Name of the stack to create from ~/docker-compose.yaml
+# LC_INITIAL_MANAGER: Private IP of this node
+# LC_OTHER_MANAGERS: Space separated list of IPs of other Swarm managers
+# LC_WORKERS: Space separated list of IPs of Swarm workers
+
+#check if docker is running
+sudo docker ps &> /dev/null || sudo service docker restart
+
+# check if $SWARM_LEADER_PRIVATE has ip then use this otherwise get ip
+export LC_INITIAL_MANAGER=${LC_INITIAL_MANAGER:-$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f8)}  #in case if the env has any consfusion about current private ip
+
+#check if swarm is already running.if yes then dont execute the following init command
+IS_ALREADY_RUNNING=$(sudo docker node list -q)
+if [ -z "$IS_ALREADY_RUNNING" ]; then
+	echo "Initializing swarm..."
+	sudo docker swarm init --advertise-addr $LC_INITIAL_MANAGER:2377 #--listen-addr $LC_INITIAL_MANAGER:2377 --advertise-addr $LC_INITIAL_MANAGER:2377
+else
+	echo "The leader node is already part of the swarm."
+fi
+
+#get swarm tokens
+export LC_MANAGER_TOKEN=$(sudo docker swarm join-token manager -q)
+export LC_WORKER_TOKEN=$(sudo docker swarm join-token worker -q)
+
+# tell swarm managers to join leader
+for j in $LC_OTHER_MANAGERS; do
+	echo "Adding $j to the swarm as a manager..."
+	ssh -n -o StrictHostKeyChecking=no -o ConnectTimeout=3 -o BatchMode=yes -o SendEnv="LC_MANAGER_TOKEN LC_INITIAL_MANAGER" ubuntu@$j "sudo docker swarm join --token $LC_MANAGER_TOKEN $LC_INITIAL_MANAGER:2377 &> /dev/null";
+done
+
+for k in $LC_WORKERS; do
+	echo "Adding $k to the swarm as a worker..."
+	ssh -n -o StrictHostKeyChecking=no -o ConnectTimeout=3 -o BatchMode=yes -o SendEnv="LC_WORKER_TOKEN LC_INITIAL_MANAGER" ubuntu@$k "sudo docker swarm join --token $LC_WORKER_TOKEN $LC_INITIAL_MANAGER:2377 &> /dev/null";
+done
+
+echo "Labeling nodes..."
+HOSTNAMES=$(sudo docker node ls --format "{{.Hostname}}")
+for name in $HOSTNAMES; do
+	echo "Labeling: $name"
+	{ sudo docker node update --label-add comp=$(echo "$name"|cut -d- -f1) $name } &> /dev/null
+done
+
+echo "Launching stack '$LC_STACK_NAME'..."
+
+sudo -E docker stack deploy -c ~/docker-compose.yaml "$LC_STACK_NAME" --with-registry-auth
