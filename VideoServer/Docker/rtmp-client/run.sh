d1a8766ea185d07dc91796be20efa3c2ffd89e46
Anton Gulenko <anton.gulenko@googlemail.com>
2018-02-02 13:49:53 +0100

    Fixed the statistics output of ffmpeg, translating \r into \n, disabled output buffering.

--- VideoServer/Docker/rtmp-client/run.sh
+++ VideoServer/Docker/rtmp-client/run.sh
@@ -22,19 +22,28 @@ else
 		test -z "$MRL_PATH" && MRL_PATH="/vod/example-11s.flv"
 		test -z "$MRL_PROTO" && MRL_PROTO="rtmp"
 		for s in $MRL_SERVER; do
-			MRL="$MRL\n$MRL_PROTO://$s:$MRL_PORT$MRL_PATH"
+			test -n "$MRL" && MRL="$MRL\n"
+			MRL="$MRL$MRL_PROTO://$s:$MRL_PORT$MRL_PATH"
 		done
 	fi
 	mapfile -t MRL_BATCH_TARGETS <<< $(echo -e "$MRL")
 fi
 
-test -z "$MRL_LOG" && MRL_LOG=/tmp/rtmp.log
-touch $MRL_LOG
+if [ -n "$MRL_LOG" ]; then
+	echo "Logging ffmpeg performance stats to file: $MRL_LOG"
+elif [ -n "$MRL_LOG_DIR" ]; then
+	echo "Logging ffmpeg performance stats to individual files in directory: $MRL_LOG_DIR"
+else
+	echo "Logging ffmpeg performance stats to standard output"
+	MRL_LOG="/dev/stdout"
+fi
 
 # Check if LOOP is a valid number
 test -z "$LOOP" && LOOP=1
 test "$LOOP" -eq "$LOOP" &> /dev/null || { echo "Warning: invalid LOOP variable '$LOOP'. Using 1."; LOOP=1; }
 test $LOOP -le 0 && INFINITE=true || INFINITE=false
+
+test -z "$MRL_LOGFILE_PREFIX" && MRL_LOGFILE_PREFIX="rtmp-client-"
 
 i=1
 ffmpeg_num=1
@@ -43,17 +52,36 @@ while $INFINITE || [ $i -le $LOOP ]; do
 	INDEX=$(($i % ${#MRL_BATCH_TARGETS[*]}))
 	MRL_TARGET="${MRL_BATCH_TARGETS[$INDEX]}"
 
+	CURRENT_LOG="$MRL_LOG"
+	if [ -z "$CURRENT_LOG" ]; then
+		if [ -n "$MRL_LOG_DIR" ]; then
+			CURRENT_LOG="$MRL_LOG_DIR/$MRL_LOGFILE_PREFIX$(date '+%Y-%m-%d.%H-%M-%S.%3N').log"
+		else
+			CURRENT_LOG="/dev/null" # Last resort to avoid syntax error
+		fi
+	fi
+
 	if $INFINITE; then
-		echo "Running ffmpeg $ffmpeg_num in infinite loop loading $MRL_TARGET..."
+		echo "Running ffmpeg $ffmpeg_num in infinite loop loading $MRL_TARGET, logging to $CURRENT_LOG..."
 	else
-		echo "Running ffmpeg $ffmpeg_num of $LOOP loading $MRL_TARGET..."
+		echo "Running ffmpeg $ffmpeg_num of $LOOP loading $MRL_TARGET, logging to $CURRENT_LOG..."
 	fi
 
 	# Execute the actual ffmpeg process. Loglevel 23 outputs performance statistics, errors and nothing else.
 	# Pipe the output to the logfile, do not output the video anywhere.
-	ffmpeg -i "$MRL_TARGET" -f null /dev/null -loglevel 23 -stats $@ &>> "$MRL_LOG" </dev/null
+	stdbuf -o128 -e128 \
+		ffmpeg -i "$MRL_TARGET" -f null /dev/null -loglevel 23 -stats $@ </dev/null |& \
+	stdbuf -i0 -eL -oL \
+		tr "\r" "\n" &>> "$CURRENT_LOG"
 
-	sleep 1 || exit 0 # Allow user to end this loop with CTRL-C
+	# ffmpeg outputs statistics on stderr, every entry is separated by \r instead of \n (\r clears the line for better experience on an interactive command line).
+	# We want to receive every entry on a single line for easier processing, therefore we use the 'tr' command to translate \r into \n characters.
+	# The 'tr' command introduces a pipe, and its output is redirected to a file, which affects the buffering behavior of both ffmpeg and tr.
+	# In order to flush the output buffers immediately, 'stdbuf' is used to configure the buffers. For 'ffmpeg', the stdout and stderr buffer is chosen very small (128 bytes) to rapidly flush the statistics.
+	# For tr, the output is buffered by line, because it introduces the regular \n line ending character.
+
+	test $? = 255 -o $? = 130 && break # ffmpeg exits with code 255 after SIGINT
+	sleep 1 || exit 0 # Allow user to end this loop with CTRL-C, if the above 'break' does not work
 	ffmpeg_num=$((ffmpeg_num+1))
 	i=$((i+1))
 done
