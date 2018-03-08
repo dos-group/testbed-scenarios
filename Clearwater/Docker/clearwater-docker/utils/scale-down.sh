216128296c8eeea0b3a1b2925b7424feafaabb71
Muhammad Imran <mimran.646726@gmail.com>
2018-03-08 12:24:52 +0100

    fixed issues in cassandra scaledown script. tested with one instance of cassandra scaled down

--- Clearwater/Docker/clearwater-docker/utils/scale-down.sh
+++ Clearwater/Docker/clearwater-docker/utils/scale-down.sh
@@ -1,33 +1,32 @@
 #/bin/bash
 
+#run this as a service so that it can periodically check for the availability of cassandra containers
 
-#Not properly tested yet. Also, run this as a service so that it can periodically check for the availability of cassandra containers
 
-cassandra_cluster_info=$(curl -L http://etcd:2379/v2/keys/clearwater/node_type_cassandra/clustering/cassandra)
-
-values=$(echo $cassandra_cluster_info | jq -rM .node.value)
-
-ips=$(echo $values | jq 'keys[]')
-
-unreachable_ips=()
-for i in $ips;
+while true;
 do
-	ping -c3 $i 2>/dev/null 1>/dev/null
-	if [ "$?" != 0 ]
+	cassandra_cluster_info=$(curl -L http://etcd:2379/v2/keys/clearwater/node_type_cassandra/clustering/cassandra)
+
+	values=$(echo $cassandra_cluster_info | jq -r .node.value)
+	ips=$(echo $values | jq -r  'keys[]')
+	unreachable_ips=()
+	for i in $ips;
+	do
+		ping -c3 $i 2>/dev/null 1>/dev/null
+		if [ $? -ne "0" ]
+		then
+			unreachable_ips+=($i)
+		fi
+	done
+	for j in ${unreachable_ips[@]};
+	do
+		values=$(echo "$values" | jq -r 'del(.['"\"$j\""'])')
+
+	done;
+
+	if [[ ${unreachable_ips[@]} ]]; 
 	then
-		unreachable_ips+=($i)
+		curl http://etcd:2379/v2/keys/clearwater/node_type_cassandra/clustering/cassandra -XPUT -d value=$values
 	fi
-done
-
-for i in ${unreachable_ips[@]};
-do
-	values=$(echo $values | jq 'del(."$i")')
-done;
-
-if [[ ${array[@]} ]]; 
-then
-	curl http://etcd:2379/v2/keys/clearwater/node_type_cassandra/clustering/cassandra -XPUT -d value=$values
-fi
-
-
+	sleep 300
 exit 0
