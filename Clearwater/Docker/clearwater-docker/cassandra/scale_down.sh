c886fc4cc3e3aa550f85b1b837697d0b37510a97
Muhammad Imran <mimran.646726@gmail.com>
2018-03-15 14:09:34 +0100

    added log info. added sleet time in start to prevent node from being removed when initializing

--- Clearwater/Docker/clearwater-docker/cassandra/scale_down.sh
+++ Clearwater/Docker/clearwater-docker/cassandra/scale_down.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
 #run this as a service so that it can periodically check for the availability of cassandra containers
+echo "########################### START ###########################" >> /var/log/scale-down-cassandra.log
+
+#in start wait for other nodes to join
+sleep 300
 
 cassandra_cluster_info=$(curl -L http://etcd:2379/v2/keys/clearwater/node_type_cassandra/clustering/cassandra)
-#echo "$(date) $cassandra_cluster_info"
+
+echo "$(date) $cassandra_cluster_info" >> /var/log/scale-down-cassandra.log
 values=$(echo $cassandra_cluster_info | jq -r .node.value)
+echo "$(date) Values in the ETCD are '$values'" >> /var/log/scale-down-cassandra.log
 ips=$(echo $values | jq -r  'keys[]')
+echo "$(date) Extracted IPs." >> /var/log/scale-down-cassandra.log
 is_value_changed=false
 for i in $ips;
 do
+	echo "$(date) Pinging $i" >> /var/log/scale-down-cassandra.log
 	ping -c3 $i 2>/dev/null 1>/dev/null
 	if [ $? -ne "0" ]
 	then
+		echo "$(date) Could not ping IP $i. Hence, removing it from ETCD" >> /var/log/scale-down-cassandra.log
 		values=$(echo "$values" | jq -r 'del(.['"\"$i\""'])')
+		echo "$(date) New values so far are: '$values'" >> /var/log/scale-down-cassandra.log
 		is_value_changed=true
 	fi
 done
 if [ "$is_value_changed" = true ]; 
 then
+	echo "$(date) Posting updated values to ETCD. The values are: $values" >> /var/log/scale-down-cassandra.log
 	curl http://etcd:2379/v2/keys/clearwater/node_type_cassandra/clustering/cassandra -XPUT -d value=$values
 fi
+echo "########################### END OF THE ITERATION ###########################" >> /var/log/scale-down-cassandra.log
