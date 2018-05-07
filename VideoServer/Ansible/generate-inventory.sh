33d8e0d6c9b084a5b1ee674cc7cdd092a121ef49
Anton Gulenko <anton.gulenko@googlemail.com>
2018-05-07 10:58:55 +0200

    Added separate playbook for the golang-based RTMP client

--- VideoServer/Ansible/generate-inventory.sh
+++ VideoServer/Ansible/generate-inventory.sh
@@ -5,7 +5,6 @@ home=`dirname $(readlink -e $0)`
 test $# = 1 || { echo "Need 1 parameter: name of the stack to generate the inventory for"; exit 1; }
 STACK="$1"
 
-"$home/../../Common/Ansible/generate-ansible-inventory.sh" "$STACK" "10.0.42" > "$home/ansible-inventory.ini"
+"$home/../../Common/Ansible/generate-ansible-inventory.sh" "$STACK" > "$home/ansible-inventory.ini"
 
 test -f "$home/ansible-inventory-extra.ini" && cat "$home/ansible-inventory-extra.ini" >> "$home/ansible-inventory.ini"
-
