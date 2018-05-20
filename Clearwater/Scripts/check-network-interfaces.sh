5c49dc56bb2f89fb3f33d77620c81b6feea12af5
Alexander Acker <alexander.acker@tu-berlin.de>
2018-05-20 19:10:47 +0200

    Added network interface checking script

---
+++ Clearwater/Scripts/check-network-interfaces.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ansible hypervisors -b -m shell -a 'docker exec -i injector tcshow -d enp2s0'
+ansible hypervisors -b -m shell -a 'docker exec -i injector tcshow -d eno1'
+ansible vms -b -m shell -a 'docker exec -i injector tcshow -d ens3'
