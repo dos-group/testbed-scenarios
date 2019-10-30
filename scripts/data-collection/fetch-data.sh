57b952b46ac7f38313ccf8a3e0766987d6f2a909
Anton Gulenko <anton.gulenko@googlemail.com>
2019-10-30 21:02:08 +0100

    Cleaned up scripts related to data collection

--- scripts/data-collection/fetch-data.sh
+++ scripts/data-collection/fetch-data.sh
@@ -9,6 +9,7 @@ while [ -e "$FETCH_DIR/$NUM" ]; do NUM=$((NUM+1)); done
 TARGET_DIR="$FETCH_DIR/$NUM"
 mkdir -p "$TARGET_DIR"
 echo "Fetching data to $TARGET_DIR"
-"$home/fetch-data.yml" -e "fetch_dir=$TARGET_DIR"
+"$home/_fetch-data.yml" -e "fetch_dir=$TARGET_DIR"
+
 #Delete all empty directories
 find $TARGET_DIR -empty -type d -delete
--- scripts/vod/restart-collector-containers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-home=`dirname $(readlink -e $0)`
-cd "$home"
-
-ansible $@ '!client' -b -m shell -a 'containers=$(docker ps -q --no-trunc --filter name=bitflow-collector); echo "Restarting $(echo "$containers" | wc -l) containers..."; for i in $containers; do docker restart $i; done'
-
