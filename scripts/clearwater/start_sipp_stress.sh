6e3299b55c7bc1a5209fbd9e879ab7d6f172b201
Anton Gulenko <anton.gulenko@googlemail.com>
2019-12-06 11:28:46 +0100

    Removed unused ansible task files

--- scripts/clearwater/start_sipp_stress.sh
+++ scripts/clearwater/start_sipp_stress.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 home=`dirname $(readlink -e $0)`
 
 duration=36000000
@@ -11,35 +10,15 @@ while [[ $# -gt 0 ]]; do
     key="$1"
 
     case $key in
-        -d|--duration)
-        duration="$2"
-        shift # past argument
-        shift # past value
-        ;;
-        --min_users)
-        min_users="$2"
-        shift # past argument
-        shift # past value
-        ;;
-        --max_users)
-        max_users="$2"
-        shift # past argument
-        shift # past value
-        ;;
-        --t_load_change)
-        t_load_change="$2"
-        shift # past argument
-        shift # past value
-        ;;
-        *)    # unknown option
-        echo "Bad parametrization"
-        exit -1
-        ;;
+        -d|--duration) duration="$2"; shift 2 ;;
+        --min_users) min_users="$2"; shift 2 ;;
+        --max_users) max_users="$2"; shift 2 ;;
+        --t_load_change) t_load_change="$2"; shift 2 ;;
+        *) echo "Unknown parameter: $key"; exit -1 ;;
     esac
 done
 
 load_args="\"--min_users $min_users --max_users $max_users -d $duration --t_load_change $t_load_change\""
-echo $load_args
+echo "Using load_args: $load_args"
 
 "$home/start_sipp_stress.yml" -e "load_args=$load_args" 
-
